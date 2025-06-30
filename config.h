9295910ce3682175c8bf19a99992ecef47e896b4
Rob Gowin <robgowin@gmail.com>
2025-06-30 11:51:30 -0400

    Build with CMake instead of autotools.

---
+++ config.h
@@ -0,0 +1,53 @@
+/* config.h */
+
+/* Use WPC address map */
+/* #undef CONFIG_WPC */
+
+/* Use 6309 extensions */
+/* #undef H6309 */
+
+/* Use readline library */
+/* #undef HAVE_READLINE */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <termios.h> header file. */
+#define HAVE_TERMIOS_H 1
+
+/* Name of package */
+#define PACKAGE "m6809-run"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "brian@oddchange.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "m6809-run"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "m6809-run 0.92"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "m6809-run"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.92"
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
+
+/* Version number of package */
+#define VERSION "0.92"
+
